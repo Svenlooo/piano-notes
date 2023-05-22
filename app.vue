f5d22561808948b20a722fa685f849a79216ee91
Sven Lochner <tools@svenlo.net>
2023-05-22 08:35:28 +0200

    add global page styles

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage/>
+      <NuxtPage class="page-wrapper" />
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+  .page-wrapper {
+    background: linear-gradient(179.6deg, map-get($colors, 'light') 1.07%, map-get($colors, 'light2') 98.55%);
+  }
+</style>
