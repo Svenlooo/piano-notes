f3093c67bd3743185137764d2150ae8e7350d3cd
Sven Lochner <tools@svenlo.net>
2023-05-25 13:36:27 +0200

    add meta tag data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no',
+            meta: [
+                { name: 'application-name', content: 'Piano Notes'},
+
+                /** Android */
+                { name: 'theme-color', content: '#FBF7F5'},
+                { name: 'mobile-web-app-capable', content: 'yes'},
+
+                /** iOS */
+                { name: 'apple-mobile-web-app-capable', content: 'yes'},
+                { name: 'apple-mobile-web-app-title', content: 'Piano Notes'},
+
+                /** Microsoft */
+                { name: 'msapplication-tooltip', content: 'Learn Piano Notes'},
+                { name: 'msapplication-starturl', content: '/'},
+                { name: 'msapplication-tap-highlight', content: 'no'},
+
+                /** UC Browser */
+                { name: 'full-screen', content: 'yes'},
+                { name: 'browser-mode', content: 'application'},
+                { name: 'viewport', content: 'uc-fitscreen=yes'},
+            ],
+        },
+    },
     css: [
         "normalize.css/normalize.css",
         "@/assets/styles/main.scss",
