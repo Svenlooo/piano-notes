4c06fe8849c09e72193cb7a47a0577f6ac9a59f2
Sven Lochner <tools@svenlo.net>
2023-05-25 13:41:07 +0200

    change scaling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no',
+            viewport: 'width=device-width, initial-scale=1, user-scalable=no',
             meta: [
                 { name: 'application-name', content: 'Piano Notes'},
 
