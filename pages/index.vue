1278740b1a7fa0bedff636fd2884eae0c01953d5
Sven Lochner <tools@svenlo.net>
2023-05-27 14:07:32 +0200

    add more StatusBar styling details

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,19 @@
 
 <script setup>
 useServerSeoMeta({
-  title: 'Piano Notes',
-  ogTitle: 'Piano Notes',
-  description: 'Learn how to read piano notes in a fun and simple way!',
-  ogDescription: 'Learn how to read piano notes in a fun and simple way!',
-  ogImage: 'https://piano-notes.netlify.app/images/titleImage.jpg',
-})
+  title: "Piano Notes",
+  ogTitle: "Piano Notes",
+  description: "Learn how to read piano notes in a fun and simple way!",
+  ogDescription: "Learn how to read piano notes in a fun and simple way!",
+  ogImage: "https://piano-notes.netlify.app/images/titleImage.jpg",
+});
 </script>
 
 <style lang="scss" scoped>
 .piano {
   position: relative;
   height: calc(100dvh - #{$navigationHeight});
-  box-shadow: 0px 3px 7px -4px rgba(0,0,0,0.75);
+  box-shadow: 0px 3px 7px -4px rgba(0, 0, 0, 0.75);
 }
 
 .statusBar {
@@ -28,5 +28,10 @@ useServerSeoMeta({
   width: 100%;
   top: 0;
   z-index: 1;
+
+  @media screen and (orientation: landscape) {
+    width: 50%;
+    top: 8px;
+  }
 }
 </style>
