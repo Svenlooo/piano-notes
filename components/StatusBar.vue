ad5867a5148e12f22b4363b2a5bb1d6ecb0d8874
Sven Lochner <tools@svenlo.net>
2023-05-27 14:00:22 +0200

    add a small animation to the statusBar Items

--- components/StatusBar.vue
+++ components/StatusBar.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="statusBar">
-    <div class="statusBar__item statusBar__item--successful" v-if="successfulAttempts > 0">
+    <div class="statusBar__item statusBar__item--successful" 
+      :class="successfulAttempts > 0 && 'visible'">
       <Icon name="ph:check-circle" />
       {{ successfulAttempts }}
     </div>
-    <div class="statusBar__item statusBar__item--unsuccessful" v-if="unsuccessfulAttempts > 0">
+    <div class="statusBar__item statusBar__item--unsuccessful"
+      :class="unsuccessfulAttempts > 0 && 'visible'">
       <Icon name="ph:x-circle" />
       {{ unsuccessfulAttempts }}
     </div>
@@ -29,13 +31,22 @@ const unsuccessfulAttempts = computed(() => store.currentGameFailedAttempts);
   justify-content: center;
 
   &__item {
+    opacity: 0;
+    width: 0;
+    height: inherit;
     padding: 0 8px;
+    transition: opacity 800ms;
 
     &--successful {
       color: var(--color-green);
     }
     &--unsuccessful {
       color: var(--color-red);
+    }
+
+    &.visible {
+      width: auto;
+      opacity: 1;
     }
   }
 }
