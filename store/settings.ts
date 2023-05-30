ea6bda5e6ef6ac9f4ab8138dc38d49c102e5983c
Sven Lochner <tools@svenlo.net>
2023-05-30 11:27:10 +0200

    type store

--- store/settings.ts
+++ store/settings.ts
@@ -3,13 +3,13 @@ import { defineStore } from 'pinia';
 export const useSettingsStore = defineStore('settings', () => {
     
     /** Add black key notes */
-    const blackKeys = ref(false);
+    const blackKeys = ref<boolean>(false);
 
     /** Show only successes / hide failures */
-    const successOnly = ref(false);
+    const successOnly = ref<boolean>(false);
 
     /** Adjust theme according to user's system settings */
-    const theme = ref(process.client && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    const theme = ref<string>(process.client && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
     /** Toggle the theme */
     const setTheme = (mode: string) => {
