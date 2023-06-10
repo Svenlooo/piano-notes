d780df1f119ef1db5595d9527b764a147a32543a
Sven <tools@svenlo.net>
2023-06-10 11:51:53 +0200

    Delete .netlify directory

--- .netlify/functions-internal/server/chunks/app/_nuxt/IconCSS-styles.0326db2b.mjs
+++
@@ -1,6 +0,0 @@
-const IconCSS_vue_vue_type_style_index_0_scoped_5ee01813_lang = "span[data-v-5ee01813]{background-color:currentColor;display:inline-block;-webkit-mask-image:var(--5fa51ebc);mask-image:var(--5fa51ebc);-webkit-mask-repeat:no-repeat;mask-repeat:no-repeat;-webkit-mask-size:100% 100%;mask-size:100% 100%;vertical-align:middle}";
-
-const IconCSSStyles_0326db2b = [IconCSS_vue_vue_type_style_index_0_scoped_5ee01813_lang];
-
-export { IconCSSStyles_0326db2b as default };
-//# sourceMappingURL=IconCSS-styles.0326db2b.mjs.map
