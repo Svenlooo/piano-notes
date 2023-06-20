4eed01e55074a4e0782dd2f055903f7f5119d340
Sven Lochner <tools@svenlo.net>
2023-06-20 12:20:15 +0200

    fix linter error

--- interfaces/GameMetric.ts
+++ interfaces/GameMetric.ts
@@ -1,15 +1,8 @@
 import { NoteLetter } from "~/interfaces/NoteLetter";
 
 export default interface GameMetric {
-
-    /** Notes with most correct plays, e.g. 80 would mean 80% */
-    mostCorrectNotes: {
-        [key: NoteLetter]: number;
-    }
-
-    /**
-     * c - win: 8, fail: 2
-     * 
-     * c - 80%
-     */
+  /** Notes with most correct plays, e.g. 80 would mean 80% */
+  notesCorrectPercentage: {
+    [K in NoteLetter]?: number;
+  };
 }
