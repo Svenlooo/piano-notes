dae8a7c4ca26947d1017554c7893af104777be26
Sven Lochner <tools@svenlo.net>
2023-06-22 14:51:05 +0200

    add placeholder charts

--- components/Game/Detail.vue
+++ components/Game/Detail.vue
@@ -6,10 +6,17 @@
     <div id="chart">
       <ClientOnly>
         <apexchart
+          type="heatmap"
+          height="350"
+          :options="heatmapChartData.chartOptions"
+          :series="heatmapChartData.series"
+        ></apexchart>
+
+        <apexchart
           type="bar"
           height="350"
-          :options="chartData.chartOptions"
-          :series="chartData.series"
+          :options="barChartData.chartOptions"
+          :series="barChartData.series"
         ></apexchart>
       </ClientOnly>
     </div>
@@ -35,21 +42,116 @@ const adjacentIndexes = computed(() => {
   ];
 });
 
-const chartData = reactive({
+const generateData = (count, yrange) => {
+  let i = 0;
+  let series = [];
+  const letters = [
+    "Cb",
+    "C",
+    "C#",
+    "Db",
+    "D",
+    "D#",
+    "Eb",
+    "E",
+    "E#",
+    "Fb",
+    "F",
+    "F#",
+    "Gb",
+    "G",
+    "G#",
+    "Ab",
+    "A",
+    "A#",
+    "Bb",
+    "B",
+    "B#",
+  ];
+  while (i < count) {
+    let x = (i + 1).toString();
+    let y =
+      Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+    series.push({
+      x: letters[i],
+      y: y,
+    });
+    i++;
+  }
+  return series;
+};
+
+const heatmapChartData = reactive({
   series: [
     {
-      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+      name: "Octave 1",
+      data: generateData(21, {
+        min: 0,
+        max: 100,
+      }),
+    },
+    {
+      name: "Octave 2",
+      data: generateData(21, {
+        min: 0,
+        max: 100,
+      }),
+    },
+    {
+      name: "Octave 3",
+      data: generateData(21, {
+        min: 0,
+        max: 100,
+      }),
+    },
+    {
+      name: "Octave 4",
+      data: generateData(21, {
+        min: 0,
+        max: 100,
+      }),
+    },
+    {
+      name: "Octave 5",
+      data: generateData(21, {
+        min: 0,
+        max: 100,
+      }),
+    },
+  ],
+  chartOptions: {
+    chart: {
+      height: 350,
+      type: "heatmap",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ["#008FFB"],
+    title: {
+      text: "HeatMap Chart (Single color)",
+    },
+  },
+});
+
+const barChartData = reactive({
+  series: [
+    {
+      data: [
+        0, 100, 80, 40, 90, 40, 50, 70, 0, 90, 40, 10, 100, 20, 56, 22, 0, 100,
+        0, 80, 10,
+      ],
     },
   ],
   chartOptions: {
     chart: {
       type: "bar",
-      height: 350,
     },
     plotOptions: {
       bar: {
         borderRadius: 4,
-        horizontal: true,
+        horizontal: false,
       },
     },
     dataLabels: {
@@ -57,16 +159,27 @@ const chartData = reactive({
     },
     xaxis: {
       categories: [
-        "South Korea",
-        "Canada",
-        "United Kingdom",
-        "Netherlands",
-        "Italy",
-        "France",
-        "Japan",
-        "United States",
-        "China",
-        "Germany",
+        "Cb",
+        "C",
+        "C#",
+        "Db",
+        "D",
+        "D#",
+        "Eb",
+        "E",
+        "E#",
+        "Fb",
+        "F",
+        "F#",
+        "Gb",
+        "G",
+        "G#",
+        "Ab",
+        "A",
+        "A#",
+        "Bb",
+        "B",
+        "B#",
       ],
     },
   },
