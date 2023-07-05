17e8ed31513c16c77a2a549fc4ede4fd79ac11d5
Sven Lochner <tools@svenlo.net>
2023-07-05 16:13:20 +0200

    implement foundation for cypress

---
+++ cypress/e2e/pages.cy.js
@@ -0,0 +1,13 @@
+describe('Page Visit Test', () => {
+  it('Visits the Highscore page', () => {
+    cy.visit('/highscore');
+  });
+
+  it('Visits the Settings page', () => {
+    cy.visit('/settings');
+  });
+
+  it('Visits the Index page', () => {
+    cy.visit('/');
+  });
+});
