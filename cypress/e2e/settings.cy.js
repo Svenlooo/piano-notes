f2c96eda97b51836d457e8289121cda3559e9600
Sven Lochner <tools@svenlo.net>
2023-07-08 12:40:27 +0200

    refactor settings test

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,35 +1,32 @@
-describe("Settings: Darkmode Toggle", () => {
-  it("Toggles the DarkMode", () => {
+describe("Settings: Option Toggles", () => {
+  beforeEach(() => {
     cy.visit("/settings");
+  });
+
+  it("Toggles the DarkMode", () => {
     cy.get('[data-cy="settingsDarkMode"]').click();
     cy.get("html").should("have.attr", "data-theme", "dark");
 
-    cy.getAllLocalStorage().then((result) => {
-      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+    cy.window().then((win) => {
+      const settings = JSON.parse(win.localStorage.getItem("settings"));
       expect(settings.theme).to.equal("dark");
     });
   });
-});
 
-describe("Settings: Black Keys Toggle", () => {
   it("Toggles Black Keys", () => {
-    cy.visit("/settings");
     cy.get('[data-cy="settingsblackKeysToggle"]').click();
 
-    cy.getAllLocalStorage().then((result) => {
-      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+    cy.window().then((win) => {
+      const settings = JSON.parse(win.localStorage.getItem("settings"));
       expect(settings.blackKeys).to.be.false;
     });
   });
-});
 
-describe("Settings: Success Only option", () => {
   it("Toggles Success option", () => {
-    cy.visit("/settings");
     cy.get('[data-cy="settingsSuccessOnly"]').click();
 
-    cy.getAllLocalStorage().then((result) => {
-      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+    cy.window().then((win) => {
+      const settings = JSON.parse(win.localStorage.getItem("settings"));
       expect(settings.successOnly).to.be.true;
     });
   });
