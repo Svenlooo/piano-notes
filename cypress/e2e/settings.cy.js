ef4bc64d751be260c7039f56ffe1d2ee9c4708f2
Sven Lochner <tools@svenlo.net>
2023-07-08 11:52:56 +0200

    update tests to check localstorage for settings page

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -3,6 +3,11 @@ describe("Settings: Darkmode Toggle", () => {
     cy.visit("/settings");
     cy.get('[data-cy="settingsDarkMode"]').click();
     cy.get("html").should("have.attr", "data-theme", "dark");
+
+    cy.getAllLocalStorage().then((result) => {
+      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+      expect(settings.theme).to.equal("dark");
+    });
   });
 });
 
@@ -10,6 +15,11 @@ describe("Settings: Black Keys Toggle", () => {
   it("Toggles Black Keys", () => {
     cy.visit("/settings");
     cy.get('[data-cy="settingsblackKeysToggle"]').click();
+
+    cy.getAllLocalStorage().then((result) => {
+      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+      expect(settings.blackKeys).to.be.false;
+    });
   });
 });
 
@@ -17,5 +27,10 @@ describe("Settings: Success Only option", () => {
   it("Toggles Success option", () => {
     cy.visit("/settings");
     cy.get('[data-cy="settingsSuccessOnly"]').click();
+
+    cy.getAllLocalStorage().then((result) => {
+      const settings = JSON.parse(result[Cypress.config().baseUrl].settings);
+      expect(settings.successOnly).to.be.true;
+    });
   });
 });
